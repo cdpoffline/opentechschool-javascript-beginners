af458246a282f61397f66577017f1bdb6c3a6d81
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-03-05 13:18:59 -0800

    changed url of git

--- bin/download_installers.sh
+++ bin/download_installers.sh
@@ -7,7 +7,9 @@ cd ../web
 mkdir -p downloads
 cd downloads
 
+
 wget -c -N https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb
+wget -c -N https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 # from https://support.google.com/chrome/answer/126299?hl=de
 wget -c -N https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7BEC81A94B-34FE-C1FE-9AF6-D023EFAA7A41%7D%26lang%3Den%26browser%3D3%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers/update2/installers/ChromeStandaloneSetup.exe
 wget -c -N https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
--- bin/update.sh
+++ bin/update.sh
@@ -6,18 +6,20 @@ cd "`dirname \"$0\"`"
 
 cd ..
 
-if [ -d "js-beginners-4h-workshop-1" ]
+folder=js-beginners-1
+
+if [ -d "$folder/.git" ]
 then
-  cd "js-beginners-4h-workshop-1"
+  cd "$folder"
   git pull
   cd ..
 else
-  git clone --depth=1 --branch=gh-pages https://github.com/OpenTechSchool/js-beginners-4h-workshop-1.git
+  git clone --depth=1 --branch=gh-pages "https://github.com/OpenTechSchool/$folder.git"
 fi
 
 cd web
 
 rm -f js-beginners-4h-workshop-1
-ln -s -T "`realpath \"../js-beginners-4h-workshop-1\"`" js-beginners-4h-workshop-1
+ln -s -T "`realpath \"../$folder\"`" js-beginners-4h-workshop-1
 
 ../bin/download_installers.sh
